e86cc6f1eec9b17e480293c01f5f9cf6cfb0cd98
Alesya Sewell <alesyadb@icloud.com>
2025-11-14 16:09:58 +0900

    Updated versipon withg modals and styling

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,17 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
-  def new
-    @list = List.new
-  end
+  # def new
+  #   @list = List.new
+  # end
 
   def create
     @list = List.new(list_params)
@@ -20,10 +22,16 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to list_path
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 
 end
